40ce177becba29dd67de3c12d5ebfaecadf27c6a
Jaelson Santos <jaelson.jua@gmail.com>
2019-09-27 09:50:47 -0300

    fix: adicionando tema

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'ui/Home/home_screen.dart';
 import 'ui/Login/login_screen.dart';
@@ -11,6 +12,19 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return LoginScreen();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark
+    ));
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Color(0xFFFF3D58),
+          textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: Colors.grey[800],
+            displayColor: Colors.grey[800]
+          )
+        ),
+        home: LoginScreen());
   }
 }
