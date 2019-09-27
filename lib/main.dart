728377568796630123ca2ee758776046dbb49208
Jaelson Santos <jaelson.jua@gmail.com>
2019-09-27 09:50:31 -0300

    fix: movendo o MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,5 @@ import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(
-    primaryColor:  Color(0xFFFF3D58),
-  ),
-  home: App()
-  ));
+void main() => runApp(App());
 
